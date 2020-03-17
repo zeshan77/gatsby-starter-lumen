99209bcf695f0328b2e173d43c5f43b77bffbf3d
Aleksander Helgaker <aleksander.helgaker@gmail.com>
2020-03-17 22:05:16 +0100

    Improve code readability.

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -8,21 +8,30 @@ const Author = () => {
   const { author } = useSiteMetadata();
   const authorHasTwitter = author.contacts.twitter !== '';
 
+  let authorSignature;
+  if (authorHasTwitter) {
+    authorSignature = (
+      <a
+        className={styles['author__bio-twitter']}
+        href={getContactHref('twitter', author.contacts.twitter)}
+        rel="noopener noreferrer"
+        target="_blank">
+        <strong>{author.name}</strong> on Twitter
+      </a>
+    );
+  } else {
+    authorSignature = (
+      <div>
+        <strong>{author.name}</strong>
+      </div>
+    );
+  }
+
   return (
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
         {author.bio}
-        { authorHasTwitter
-          ? <a
-            className={styles['author__bio-twitter']}
-            href={getContactHref('twitter', author.contacts.twitter)}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <strong>{author.name}</strong> on Twitter
-          </a>
-          : <div><strong>{author.name}</strong></div>
-        }
+        {authorSignature}
       </p>
     </div>
   );
